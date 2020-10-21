df2cdbf9ba91306c3a9240a7109c7d9e88032f95
OscarW1 <162868@chrome.escg.ac.uk>
2020-10-21 15:01:54 +0100

    Added a movement script and some more edited art pieces

--- Acc ColourTheoryGame/Assets/Scripts/PlayerMovement.cs
+++ Acc ColourTheoryGame/Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,44 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Transform playerTransform;
+    public float playerSpeed;
+
+    private float move;
+    private Rigidbody2D rb;
+    
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        move = Input.GetAxis("Horizontal");
+
+        rb.velocity = new Vector2(move * playerSpeed, rb.velocity.y);
+
+        if (move < 0)
+        {
+            playerTransform.eulerAngles = new Vector3(0, 180, 0);
+        }
+        else if (move > 0)
+        {
+            playerTransform.eulerAngles = new Vector3(0, 0, 0);
+        }
+
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+          
+            if (Input. GetKey(KeyCode.Space))
+            {
+                rb.AddForce(Vector3.up * (rb.mass * rb.gravityScale * 20f * 10f));
+                Debug.Log("Working jump you twat");
+            }
+        }
     }
 }
