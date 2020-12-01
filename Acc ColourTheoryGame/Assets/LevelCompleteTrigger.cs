41024fd694828c6e29b67aa6706495b792e5a006
OscarW1 <162868@chrome.escg.ac.uk>
2020-12-01 15:57:01 +0000

    Added a level complete screen and script as well as another checkpoint

---
+++ Acc ColourTheoryGame/Assets/LevelCompleteTrigger.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelCompleteTrigger : MonoBehaviour
+{
+    public int buildIndex;
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        SceneManager.LoadScene(buildIndex);
+        
+    }
+}
